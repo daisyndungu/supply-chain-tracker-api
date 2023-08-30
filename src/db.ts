073d7c73c6df6f333bfa690b6fa8bb6aeb2071d2
Daisy Ndung'u <daisyndungu@microsoft.com>
2023-08-30 15:16:22 +0300

    move database connection url details to env file

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
+import { load } from 'ts-dotenv';
 
-const MONGODB_URI = 'mongodb://localhost/supplychain';
+const env = load({
+    MONGODB_URL: String,
+    MONGODB_PORT: Number,
+    MONGODB_DATABASENAME: String
+})
+
+const MONGODB_URI = `${env.MONGODB_URL}:${env.MONGODB_PORT}/${env.MONGODB_DATABASENAME}`;
 
 export const connectToDatabase = async (): Promise<void> => {
   try {
